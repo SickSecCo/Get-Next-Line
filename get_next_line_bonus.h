08ee777950dd507baf2fe6a1c10865e26e1f546a
SickSecCo <deasy3067@gmail.com>
2023-04-21 18:44:02 +0200

    ok

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fforlini <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 16:17:05 by fforlini          #+#    #+#             */
+/*   Updated: 2022/01/26 16:18:01 by fforlini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_substr(char *s, unsigned int start, size_t len);
+char		*ft_strchr(char *s, int c);
+char		*ft_strdup(const char *s1);
+
+#endif
